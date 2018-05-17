97bdbe715b230e360f8e91968d7ea4e8be99b7e6
Eldho <eldho@live.in>
2018-05-17 14:03:59 +0530

    Move code to Easy permissions and other minor improvements

--- app/src/main/java/com/example/eldho/googlelocationapinew/MainActivity.java
+++ app/src/main/java/com/example/eldho/googlelocationapinew/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.eldho.googlelocationapinew;
 
 import android.Manifest;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
@@ -16,6 +15,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -23,8 +23,14 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity {
-    private int LOCATION_PERMISSION_CODE = 1;
+import java.util.List;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+    private static final int LOCATION_PERMISSION_CODE = 1;
     TextView textLocationTv;
     Button startBtn, stopBtn;
     LocationRequest locationRequest;
@@ -52,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
                 fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
                 //code ends here
             } else {
-                requestStoragePermission();
+                requestLocationPermission();
             }
         }
 
@@ -62,13 +68,13 @@ public class MainActivity extends AppCompatActivity {
 
                 //If permission is granted the code goes in here
                 if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
+                    requestLocationPermission();
                     return;
                 }
                 fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
-                        startBtn.setEnabled(false);
-                        stopBtn.setEnabled(true);
-                        //code ends here
+                startBtn.setEnabled(false);
+                stopBtn.setEnabled(true);
+                //code ends here
             }
         });
 
@@ -83,51 +89,68 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void requestStoragePermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                Manifest.permission.ACCESS_FINE_LOCATION))  // this is to show Alertdialog when user deny permission
-        {
-            new AlertDialog.Builder(this)
-                    .setTitle("Permission needed")
-                    .setMessage("This permission is needed to write / read from storage") // Alert dialog to show the use of this permission
-                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions(MainActivity.this,
-                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
-                        }
-                    })
-                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    })
-                    .create().show();
+    //Permission request code starts here
+    @AfterPermissionGranted(LOCATION_PERMISSION_CODE)
+    private void requestLocationPermission() {
+        String[] perms = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+        if (EasyPermissions.hasPermissions(this, perms)) { //check permission is granted or not
+
+            //code if permission is granted
+            Toast.makeText(this, "Granted", Toast.LENGTH_SHORT).show();
+
         } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
+
+            /**Rationalte dialog write here*/
+            EasyPermissions.requestPermissions(this, "Please grand location permission",
+                    LOCATION_PERMISSION_CODE, perms);
         }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == LOCATION_PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Log.i("onRequestPermiResult:", " Granded");
-            } else {
-                Log.i("onRequestPermiResult:", "Denied");
-            }
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+
+        /** if some permissions denys the user will SEND TO SETTINGS of the app to manually grand the permission*/
+
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
         }
     }
+
+    /**
+     * Code when user came back from the settings <Optional/> >
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE) {
+            // Do something after user returned from app settings screen, like showing a Toast.
+            Toast.makeText(this, "Welcome Back", Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    //permission section ends here
+
+
     private void locationCallback() {
         locationCallback = new LocationCallback(){
             @Override
             public void onLocationResult(LocationResult locationResult) {
-                for (Location location:locationResult.getLocations()){
-                textLocationTv.setText(String.valueOf(location.getLatitude()
-                        + "/"
-                        + location.getLongitude() ));
+                for (Location location:locationResult.getLocations()) {
+                    textLocationTv.setText(String.valueOf(location.getLatitude()
+                            + "/"
+                            + location.getLongitude() ));
                 }
             }
         };
